2e39aa32841a2ed736e2098459e30b1d90e49b8d
captainwc <wddjwk@qq.com>
2023-02-27 18:45:49 +0800

    PA1-建议调试器完结

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -23,7 +23,19 @@ const char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
+#define REGISTERS_PER_LINE 4
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+
 void isa_reg_display() {
+  int length = ARRLEN(regs);
+  int i = 0;
+  printf("=========寄存器信息=========\n");
+  for (i = 0; i < length; i+= REGISTERS_PER_LINE){
+    for (int j = i; j < MIN(length, i + REGISTERS_PER_LINE); ++j){
+      printf("\e[1;36m%3s:\e[0m %#12x | ", regs[j], cpu.gpr[j]._32);
+    }
+    printf("\n");
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
